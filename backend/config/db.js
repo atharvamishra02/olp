9bd4cdaf4e1825ea08b5d1711341e85315e4535e
Atharva Mishra <mishraatharva756@gmail.com>
2025-07-17 17:15:31 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/online_learning';
+const MONGO_URI =
+  process.env.MONGO_URI ||
+  "mongodb+srv://mishraatharva756:0SVWdLjAbnENzPKK@k.ttiyrcc.mongodb.net/online_learning?retryWrites=true&w=majority&appName=k";
 
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   } catch (err) {
-    console.error('MongoDB connection error:', err);
+    console.error("MongoDB connection error:", err);
     process.exit(1);
   }
 };
 
-module.exports = connectDB; 
+module.exports = connectDB;
