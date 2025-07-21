751a4c460fc7d706be9ddb2371f46aae7258b577
Atharva Mishra <mishraatharva756@gmail.com>
2025-07-21 15:52:14 +0530

    vercel changes

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [tailwindcss(),react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
 })
